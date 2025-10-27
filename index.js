b4eceffb4a642d321f945d2f43482b8973f2a15f
Vishant <vishantkumar755@gmail.com>
2025-10-27 13:14:44 +0530

    Initial commit: JSON Server backend

---
+++ index.js
@@ -0,0 +1,15 @@
+import jsonServer from "json-server";
+import cors from "cors";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
